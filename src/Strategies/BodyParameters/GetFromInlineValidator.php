d3384a99ea929711e3d7fb7c7c804545e9d5d017
geangontijo <geangontijo07@gmail.com>
2025-02-15 14:58:47 -0300

    Criando regra no BodyParametersGetFromInlineValidator para validar o endpoint GET somente caso ele tenha o attribute DocumentateInBodyParams.

--- src/Strategies/BodyParameters/GetFromInlineValidator.php
+++ src/Strategies/BodyParameters/GetFromInlineValidator.php
@@ -2,17 +2,25 @@
 
 namespace AjCastro\ScribeTdd\Strategies\BodyParameters;
 
+use AjCastro\ScribeTdd\Strategies\QueryParameters\GetFromInlineValidator as QueryParametersGetFromInlineValidator;
 use Knuckles\Camel\Extraction\ExtractedEndpointData;
 use Knuckles\Scribe\Extracting\Strategies\BodyParameters\GetFromInlineValidator as BaseGetFromInlineValidator;
+use PhpParser\Node;
 
 class GetFromInlineValidator extends BaseGetFromInlineValidator
 {
     public function __invoke(ExtractedEndpointData $endpointData, array $routeRules = []): ?array
     {
-        if (in_array('GET', $endpointData->route->methods())) {
-            return [];
-        }
-
+        $this->endpointData = $endpointData;
         return parent::__invoke($endpointData, $routeRules);
+    }
+
+    protected function isValidationStatementMeantForThisStrategy(Node $validationStatement): bool
+    {
+        if (!in_array('GET', $this->endpointData->route->methods())) {
+            return true;
+        }
+        
+        return QueryParametersGetFromInlineValidator::shouldUseBodyParameters($this->endpointData);
     }
 }
