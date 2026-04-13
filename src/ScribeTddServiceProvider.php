d2eb2f92ae854c3da0301bc62b914013af3afe9f
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2026-04-13 09:45:42 -0300

    Remove restrição do endpoint up

--- src/ScribeTddServiceProvider.php
+++ src/ScribeTddServiceProvider.php
@@ -23,7 +23,6 @@ class ScribeTddServiceProvider extends ServiceProvider
         $excludeRoutes = Config::get('scribe.routes.0.exclude');
         $excludeRoutes[] = '_ignition/*';
         $excludeRoutes[] = 'oauth/*';
-        $excludeRoutes[] = 'up';
         $excludeRoutes[] = 'storage/*';
 
         Config::set('scribe.routes.0.match.prefixes', ['*']);
